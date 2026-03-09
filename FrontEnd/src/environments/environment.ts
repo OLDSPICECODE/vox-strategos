9ebb56e6c4b46230c462ff8409992ee6d75c840b
OLDSPICECODE <mauricio.estefanero@ucsp.edu.pe>
2026-03-09 15:49:45 -0500

    sd

--- FrontEnd/src/environments/environment.ts
+++ FrontEnd/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://lino-glomerate-danielle.ngrok-free.dev' // 👈 Cambia esto por la URL de tu API
+  apiUrl: '/api' // 👈 IMPORTANTE: Esto le dice a Angular "usa la misma URL de ngrok"
 };
